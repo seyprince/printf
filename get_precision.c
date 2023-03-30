333f18d57f8943c775128d701ab6357d860bf287
seyprince <princesey@outlook.com>
2023-03-30 20:44:34 +0000

    get_precision

--- get_precision.c
+++ get_precision.c
@@ -12,30 +12,30 @@ int get_precision(const char *format, int *i, va_list list)
 {
     int curr_i = *i + 1;
     int precision = -1;
-    char current_char = format[curr_i];
 
-    if (current_char == '.')
+    if (format[curr_i] != '.')
+        return (precision);
+
+    precision = 0;
+
+    curr_i++;
+
+    if (format[curr_i] == '*')
     {
-        precision = 0;
-        current_char = format[++curr_i];
-
-        if (current_char == '*')
+        curr_i++;
+        precision = va_arg(list, int);
+    }
+    else
+    {
+        for (; is_digit(format[curr_i]); curr_i++)
         {
-            curr_i++;
-            precision = va_arg(list, int);
-        }
-        else
-        {
-            while (is_digit(current_char))
-            {
-                precision = (precision * 10) + (current_char - '0');
-                current_char = format[++curr_i];
-            }
+            precision *= 10;
+            precision += format[curr_i] - '0';
         }
     }
 
     *i = curr_i - 1;
 
-    return precision;
+    return (precision);
 }
 
